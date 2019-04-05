1bd91fe987839be497ef609098a21839a590ae09
rinkei <rinkei.krt6006@gmail.com>
2019-04-06 02:05:10 +0900

    changeSendWorkspace Nekohouse->rinkeiWorkspace

--- scripts/selectnotice.js
+++ scripts/selectnotice.js
@@ -1,3 +1,4 @@
+const request = require("request")
 module.exports = robot => {
   robot.hear(/\#めし/i,msg =>{
     robot.send({room:"#めしぃプロ"},
@@ -9,9 +10,16 @@ module.exports = robot => {
     //"CHLD2ADPX" === testWorkSpace roomID
     //"CD04KEZ6V" === nekohouse times_rinkei
     if((msg.message.room === "CHLD2ADPX" || msg.message.room === "CD04KEZ6V") && !msg.message.rawMessage.subtype){
-      robot.send({room:"#times_rinkei_human"},
-      `https://${process.env.SLACK_WORKSPACE_URL}/archives/${msg.message.rawMessage.channel}/p${msg.message.rawMessage.ts.toString().replace(/\./,"")}`
-      )
+      let text = 
+      `${msg.message.rawMessage.user.name} ${msg.message.rawMessage.text} `+
+      ` https://${process.env.SLACK_WORKSPACE_URL}/archives/`+
+      `${msg.message.rawMessage.channel}/p`+
+      `${msg.message.rawMessage.ts.toString().replace(/\./,"")} `
+      request.get({
+        url: `http://192.168.0.62:9002/?{%22channel%22:%22times_rinkei_human%22,%22text%22:%22${encodeURIComponent(text)}%22}`
+      },(error, response, body)=>{
+        console.log(body)
+      })
     }
   })
 }
